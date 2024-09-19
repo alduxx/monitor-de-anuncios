c4712816b64013336457b7d01ed1e57fb8f6f418
Aldo Monteiro <alduxx@gmail.com>
2024-09-19 10:43:30 -0300

    Update telegram.py
    Adicionado notificacao para envio de msg de erro

--- src/notifica/telegram.py
+++ src/notifica/telegram.py
@@ -38,3 +38,32 @@ def envia_notificacao(anuncio):
     else:
         print('NOTIFICA: Falha ao enviar a mensagem Telegram.')
         return 0
+
+
+
+def envia_notificacao_erro(msg):
+    load_dotenv()
+    token = os.getenv('TELEGRAM_TOKEN')
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
+
+    TEXTO = f"""⛔️  ERRO!!
+{msg}
+    """
+
+    message_data = {
+        'chat_id': chat_id,
+        'text': TEXTO,
+        'parse_mode': 'HTML'
+    }
+
+    URL = f'https://api.telegram.org/bot{token}/sendMessage'
+
+    response = requests.post(URL, data=message_data)
+
+    if response.status_code == 200:
+        print('NOTIFICA: Mensagem de erro enviada com sucesso!')
+        # TODO: update bd
+        return 1
+    else:
+        print('NOTIFICA: Falha ao enviar a mensagem de erro Telegram.')
+        return 0
